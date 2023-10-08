3e31df1db6f60bf7d969bb46180295808846a605
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-10-08 16:11:32 +0300

    navigationState

--- app/src/main/java/com/ilyakoz/vknewsapp/NavigationItem.kt
+++ app/src/main/java/com/ilyakoz/vknewsapp/NavigationItem.kt
@@ -5,22 +5,27 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.ilyakoz.vknewsapp.navigation.Screen
 
 sealed class NavigationItem(
+    val screen: Screen,
     val titleResId: Int,
     val icon: ImageVector
 ) {
     object Home : NavigationItem(
+        screen = Screen.NewsFeed,
         titleResId = R.string.navigation_item_main,
         icon = Icons.Filled.Home
     )
 
     object Favourite : NavigationItem(
+        screen = Screen.Favourite,
         titleResId = R.string.navigation_item_favourite,
         icon = Icons.Filled.Favorite
     )
 
     object Profile : NavigationItem(
+        screen = Screen.Profile,
         titleResId = R.string.navigation_item_profile,
         icon = Icons.Filled.AccountCircle
     )
