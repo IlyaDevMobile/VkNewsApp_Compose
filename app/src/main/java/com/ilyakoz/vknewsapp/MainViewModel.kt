a537e407eaa62e578bb7eb3e10e6f11d0ae70f2b
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-10-13 00:15:20 +0300

    Screen state

--- app/src/main/java/com/ilyakoz/vknewsapp/MainViewModel.kt
+++ app/src/main/java/com/ilyakoz/vknewsapp/MainViewModel.kt
@@ -5,8 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ilyakoz.vknewsapp.domain.FeedPost
 import com.ilyakoz.vknewsapp.domain.StatisticItem
-import java.util.Collections
-import java.util.Collections.replaceAll
+import com.ilyakoz.vknewsapp.ui.HomeScreenState
 
 class MainViewModel : ViewModel() {
 
@@ -15,8 +14,12 @@ class MainViewModel : ViewModel() {
             add(FeedPost(id = it ))
         }
     }
-    private val _feedPosts = MutableLiveData<List<FeedPost>>(sourceList)
-    val feedPosts: LiveData<List<FeedPost>> = _feedPosts
+
+    private val initialState  = HomeScreenState.Posts(posts = sourceList)
+
+
+    private val _screenState = MutableLiveData<HomeScreenState>(initialState)
+    val screenState: LiveData<HomeScreenState> = _screenState
 
 
 
@@ -35,7 +38,7 @@ class MainViewModel : ViewModel() {
 
 
     fun updateCount(feedPost: FeedPost, item: StatisticItem) {
-        val oldPosts = feedPosts.value?.toMutableList() ?: throw IllegalArgumentException()
+        val oldPosts = screenState.value?.toMutableList() ?: mutableListOf()
         val oldStatistics = feedPost.statistics
         val newStatistics = oldStatistics.toMutableList().apply {
             replaceAll { oldItem ->
@@ -47,7 +50,7 @@ class MainViewModel : ViewModel() {
             }
         }
         val newFeedPost = feedPost.copy(statistics = newStatistics)
-        _feedPosts.value = oldPosts.apply {
+        _screenState.value = oldPosts.apply {
             replaceAll {
                 if (it.id == newFeedPost.id) {
                     newFeedPost
@@ -61,8 +64,8 @@ class MainViewModel : ViewModel() {
 
 
     fun remove(feedPost: FeedPost){
-        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldPosts = screenState.value?.toMutableList() ?: mutableListOf()
         oldPosts.remove(feedPost)
-        _feedPosts.value = oldPosts
+        _screenState.value = oldPosts
     }
 }
