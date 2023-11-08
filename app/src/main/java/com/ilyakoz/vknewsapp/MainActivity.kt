e087258a4d2be0c9bf15bab83495f82abe42cd3a
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-11-09 00:02:24 +0300

    getVKAuthActivityResultContract

--- app/src/main/java/com/ilyakoz/vknewsapp/MainActivity.kt
+++ app/src/main/java/com/ilyakoz/vknewsapp/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VkNewsAppTheme {
+            VkNewsAppTheme {ff
 
                 val launcher = rememberLauncherForActivityResult(
                     contract = VK.getVKAuthActivityResultContract()
